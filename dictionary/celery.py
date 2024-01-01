6250b4f4fe078da499cb7d4cc9b52f21fc70dc63
Yolshin <elshin195@gmail.com>
2024-01-01 20:34:14 +0700

    Version: 2.1 - bugfix

--- dictionary/celery.py
+++ dictionary/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dictionary.settings')
 
-app = Celery('dictionary_service')
+app = Celery('dictionary')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
